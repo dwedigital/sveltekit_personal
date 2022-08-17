15dfd0570598947f06235a6b2b1ceb2240502244
dwedigital <dave@dwedigital.com>
2022-08-17 21:44:57 +0100

    testing env

--- src/routes/api/blog/index.js
+++ src/routes/api/blog/index.js
@@ -1,7 +1,7 @@
 export async function GET() {
 	const options = {
 		headers: {
-			Authorization: `Bearer ${process.env.VITE_STRAPI_API_KEY}`
+			Authorization: `Bearer dbd4b3453d217f9ad8738f72f0e520c5fa778b34dc17cf40922c026589c69c5ffd530afc5c696f7bd840f8615b2806ece384163b0cae76ac17054ea49290a6436d4e2788d35984bcc267446a36754ee630d1bf77a9bbe7d679a31cfc615112f6269d6aa7263711cf7a4ef9e92c51a7152444d1c3abd03e33b4da0a83f8d96c71`
 		}
 	};
 	const response = await fetch(
