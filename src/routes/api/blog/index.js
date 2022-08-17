135bfbb40ffb7c6b71484927119ae19800037be3
dwedigital <dave@dwedigital.com>
2022-08-17 21:32:04 +0100

    testing env

--- src/routes/api/blog/index.js
+++ src/routes/api/blog/index.js
@@ -1,7 +1,7 @@
 export async function GET() {
 	const options = {
 		headers: {
-			Authorization: `Bearer ${import.meta.env.VITE_STRAPI_API_KEY}`
+			Authorization: `Bearer ${process.env.VITE_STRAPI_API_KEY}`
 		}
 	};
 	const response = await fetch(
